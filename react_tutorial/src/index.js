e0d3c71daa668888b9df398c6651bc50b5cf055b
Javier Contreras <contreras@hpe.com>
2020-01-30 13:17:38 -0600

    Header and Footer

--- react_tutorial/src/index.js
+++ react_tutorial/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css' //Se importa primero para que index.css haga override de las clases de bootstrap, en caso de querer tener algun custom
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
